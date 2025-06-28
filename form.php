6d4fdf202ec74a2c74857019c70efdbcbf64028c
Ayesha Khatun <ayesha@example.com>
2025-06-28 11:49:35 -0700

    form

---
+++ form.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Curd Opearations</title>
+</head>
+<body>
+
+    <div class="container">
+
+        <div class="title">
+            Registration Form
+        </div>
+
+        <div class="input-field">
+           <label> First Name </label>
+           <input type="text" class="input">
+        </div>
+   
+       <div class="input-field">
+           <label> Last Name </label>
+           <input type="text" class="input">
+        </div>
+
+       
+
+        <div class="input-field">
+           <label> Password </label>
+           <input type="password" class="input">
+        </div>
+
+        <div class="input-field">
+           <label> Confirm Password </label>
+           <input type="password" class="input">
+        </div>
+
+         <div class="input-field">
+            <label> Gender</label>
+
+            <Select>
+                <Option>Select</Option>
+                <Option>Men</Option>
+                <Option>Women</Option>
+            </Select>
+           
+        </div>
+
+        <div class="input-field">
+           <label> Email </label>
+           <input type="text" class="input">
+        </div>
+
+        <div class="input-field">
+           <label> Phone </label>
+           <input type="text" class="input">
+        </div>
+
+         <div class="input-field">
+           <label> Address </label>
+          <textarea></textarea>
+        </div>
+        
+        <div class="input-field-terms">
+           <label class="check"> 
+                  <input type="checkbox">
+                  <span class="checkmark"> </span>
+           </label>
+           <p> Agree to term and Conditions
+            
+           </p>
+          
+        </div>
+
+
+
+</div>
+    
+</body>
+</html>
