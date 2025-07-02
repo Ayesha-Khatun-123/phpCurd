ad725bdcf5fc05a637bd28e3d40d9c2bb16a0c93
Ayesha Khatun <ayesha@example.com>
2025-07-01 17:28:57 -0700

    form

--- connection.php
+++ connection.php
@@ -1,14 +1,18 @@
 <?php
+ //error_reporting(0); eta error hole warning take show korab ena only dekhabe connaction failed
 $servername= "localhost";
 $username="root";
 $password="Ayesha@2003";
 $dbname ="responsiveform";
- $conn = mysqli_connect($servername,$username,$password,$dbname );
+$conn = mysqli_connect($servername,$username,$password,$dbname );
+
  if($conn){
-    echo "Connection Ok";
+    //echo " Connection Establish";
 
  }
  else{
-    echo "Cannot Connect";
+    echo " Connection Failed"; 
+    
  }
+
 ?>
--- form.php
+++ form.php
@@ -1,3 +1,5 @@
+<?php include("connection.php")
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,6 +11,7 @@
 <body>
 
     <div class="container">
+      <form  action="" method="POST">
 
         <div class="title">
             Registration Form
@@ -17,56 +20,55 @@
         <div class="form">
         <div class="input-field">
            <label> First Name </label>
-           <input type="text" class="input">
+           <input type="text" class="input" name="fname" required>
         </div>
    
        <div class="input-field">
            <label> Last Name </label>
-           <input type="text" class="input">
+           <input type="text" class="input" name="lname" required>
         </div>
 
        
 
         <div class="input-field">
            <label> Password </label>
-           <input type="password" class="input">
+           <input type="password" class="input" name="password" required>
         </div>
 
         <div class="input-field">
            <label> Confirm Password </label>
-           <input type="password" class="input">
+           <input type="password" class="input" name="conpassword" required>
         </div>
 
          <div class="input-field">
-            <label> Gender</label>
+  <label>Gender</label>
+  <select class="selectbox" name="gender" required>
+    <option value="" disabled selected>Select</option>
+    <option value="Men">Men</option>
+    <option value="Women">Women</option>
+  </select>
+</div>
 
-            <Select class="selectbox">
-                <Option>Select</Option>
-                <Option>Men</Option>
-                <Option>Women</Option>
-            </Select>
-           
-        </div>
 
         <div class="input-field">
            <label> Email Address</label>
-           <input type="text" class="input">
+           <input type="text" class="input" name="email" required>
         </div>
 
         <div class="input-field">
            <label> Phone Number</label>
-           <input type="text" class="input">
+           <input type="text" class="input" name="phone" maxlength="10" required>
         </div>
 
          <div class="input-field">
            <label> Address </label>
-          <textarea class="textarea"></textarea>
+          <textarea class="textarea" name="address"></textarea required>
         </div>
       
  <div class="input-field-terms">
   <div class="checkbox-wrapper">
     <label class="custom-checkbox">
-      <input type="checkbox" >
+      <input type="checkbox"  required>
       <span class="checkmark"></span>
     </label>
     <p>Agree to terms and Conditions</p>
@@ -75,13 +77,45 @@
 
 
   <div class="input-field">
-    <input type="submit" value="Register" class="btn">
+    <input type="submit" value="Register" class="btn" name="Register"required>
   </div>
 </div>
 
 
 </div>
+</form>
 </div>
     
 </body>
 </html>
+
+<?php
+if(isset($_POST['Register'])){
+   $fname= $_POST['fname'];
+   $lname= $_POST['lname'];
+   $pass= $_POST['password'];
+   $conpass= $_POST['conpassword'];
+   $gen= $_POST['gender'];
+   $email= $_POST['email'];
+   $phone= $_POST['phone'];
+   $add= $_POST['address'];
+
+   if($fname !="" && $lname !="" && $pass!="" &&  $conpass!="" && $gen !="" && $email !="" && $phone !="" && $add!="")
+   {
+
+     $query ="INSERT INTO form VALUES('$fname','$lname','$pass','$conpass','$gen','$email','$phone','$add')";
+    $data =  mysqli_query($conn ,$query);
+
+    if($data){
+      echo "Data Inserted Succesfully";
+    }
+    else{
+      "Data Cannot Inserted";
+    }
+   }
+   else{
+      echo "<script> alert('Please Fill the Form');</script>";
+   }
+}
+
+?>
