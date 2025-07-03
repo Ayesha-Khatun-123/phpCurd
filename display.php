91e8cf0206ba25217c9f716777449cdfca2bd5b4
Ayesha Khatun <ayesha@example.com>
2025-07-03 09:24:20 -0700

    update data

--- display.php
+++ display.php
@@ -1,3 +1,27 @@
+<html>
+    <head>
+        <title>Display</title>
+        <style>
+            body{
+                background:plum;
+            }
+            table{
+                background:white;
+            }
+            .up{
+                background-color:green;
+                color:white;
+                border-radius:10px;
+                border:0;
+                outline:none;
+                height:25px;
+                width:86px;
+                cursor:pointer;
+            }
+       </style>
+</head>
+</html>
+
 <?php 
 include("connection.php");
 error_reporting(0);
@@ -7,10 +31,47 @@ $query = "SELECT * FROM FORM";
 $data = mysqli_query($conn, $query);
 $total = mysqli_num_rows($data);
 
-if ($total > 0) {
-    $result = mysqli_fetch_assoc($data);
-    echo $result['fname'] . " " . $result['lname']." ".$result['gender'];
+if ($total != 0) {
+
+    ?>
+    <h2 align="center" ><mark>Displaying All Record </mark></h2>
+   <center> <table border="3" cellspacing="10" width="100%" align="center" >
+        <tr>
+             <th width="5%"> Id</th>
+            <th width="8%"> First Name</th>
+            <th width="8%"> Last Name</th>
+            <th width="10%"> Gender </th>
+            <th width="20%"> Email Address </th>
+            <th width="10%"> Phone Number </th>
+            <th width="24%">Address </th>
+             <th width="15%">Operations</th>
+       </tr>
+    <?php
+
+   while( $result = mysqli_fetch_assoc($data)){
+  
+     echo "<tr>
+             <td>".$result['id']."</td>
+              <td>".$result['fname']."</td>
+              <td>".$result['lname']." </td>
+              <td>".$result['gender']." </td>
+             <td> ".$result['email']. "</td>
+             <td>".$result['phone']." </td>
+             <td>".$result['address']." </td>
+              
+           <td><a href='update_design.php?id=$result[id]'><input type='submit' value='Update' class=up </a></td>
+
+        
+         </tr>";
+     
+   }
+   
 } else {
     echo "No records found.";
 }
+
+
 ?>
+</table>
+</center>
+
--- update_design.php
+++ update_design.php
@@ -1,4 +1,18 @@
-<?php include("connection.php")
+<?php include("connection.php");
+//$id=$_GET['id'];
+$id = isset($_GET['id']) ? $_GET['id'] : '';
+if ($id == '') {
+    echo "<script>alert('No ID provided.'); window.location.href='display.php';</script>";
+    exit();
+}
+
+$query = "SELECT * FROM FORM where id='$id'";
+
+$data = mysqli_query($conn, $query);
+$total = mysqli_num_rows($data);
+$result = mysqli_fetch_assoc($data);
+  
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,55 +28,75 @@
       <form  action="" method="POST">
 
         <div class="title">
-            Registration Form
+            Update Student Details
         </div>
  
         <div class="form">
         <div class="input-field">
            <label> First Name </label>
-           <input type="text" class="input" name="fname" required>
+           <input type="text" value="<?php echo $result['fname'];?>" class="input" name="fname" required>
         </div>
    
        <div class="input-field">
            <label> Last Name </label>
-           <input type="text" class="input" name="lname" required>
+           <input type="text" value="<?php echo $result['lname'];?>" class="input" name="lname" required>
         </div>
 
        
 
         <div class="input-field">
            <label> Password </label>
-           <input type="password" class="input" name="password" required>
+           <input type="password"  value="<?php echo $result['password'];?>" class="input" name="password" required>
         </div>
 
         <div class="input-field">
            <label> Confirm Password </label>
-           <input type="password" class="input" name="conpassword" required>
+           <input type="password" value="<?php echo $result['conpassword'];?>"class="input" name="conpassword" required>
         </div>
 
          <div class="input-field">
   <label>Gender</label>
   <select class="selectbox" name="gender" required>
     <option value="" disabled selected>Select</option>
-    <option value="Men">Men</option>
-    <option value="Women">Women</option>
+    <option value="Men"
+    <?php 
+     if($result['gender'] == 'Men'){
+         echo 'selected';
+     }
+    ?>
+    >Men</option>
+
+    <option value="Women"
+
+    <?php 
+     if($result['gender'] == 'Women'){
+         echo 'selected';
+     }
+    ?>
+    
+    >Women</option>
   </select>
 </div>
 
 
         <div class="input-field">
            <label> Email Address</label>
-           <input type="text" class="input" name="email" required>
+           <input type="text"value="<?php echo $result['email'];?>" class="input" name="email" required>
         </div>
 
         <div class="input-field">
            <label> Phone Number</label>
-           <input type="text" class="input" name="phone" maxlength="10" required>
+           <input type="text" value="<?php echo $result['phone'];?>" class="input"value="<?php echo $result['email'];?>" name="phone" maxlength="10" required>
         </div>
 
          <div class="input-field">
            <label> Address </label>
-          <textarea class="textarea" name="address"></textarea required>
+          <textarea class="textarea" name="address" required>
+          <?php
+          echo  $result['address'];?>
+          
+        
+        </textarea>
         </div>
       
  <div class="input-field-terms">
@@ -77,7 +111,7 @@
 
 
   <div class="input-field">
-    <input type="submit" value="Register" class="btn" name="Register"required>
+    <input type="submit" value="Update Details" class="btn" name="update"required>
   </div>
 </div>
 
@@ -90,7 +124,7 @@
 </html>
 
 <?php
-if(isset($_POST['Register'])){
+if(isset($_POST['update'])){
    $fname= $_POST['fname'];
    $lname= $_POST['lname'];
    $pass= $_POST['password'];
@@ -103,15 +137,18 @@ if(isset($_POST['Register'])){
    if($fname !="" && $lname !="" && $pass!="" &&  $conpass!="" && $gen !="" && $email !="" && $phone !="" && $add!="")
    {
 
-    $query ="INSERT INTO form (fname, lname, password, conpassword, gender, email, phone, address) 
-         VALUES('$fname','$lname','$pass','$conpass','$gen','$email','$phone','$add')";
+    $query ="UPDATE form  set fname='$fname',lname='$lname',password='$pass',conpassword='$conpass',gender='$gen',email='$email',phone='$phone',address='$add' where id='$id'";
     $data =  mysqli_query($conn ,$query);
 
     if($data){
-      echo "Data Inserted Succesfully";
+      echo "<script>alert('Data Updated Succesfully')</script>";
+      ?>
+      <meta http-equiv = "refresh" content = "0; url = http://localhost/PhpCurd/display.php" />
+
+      <?php
     }
     else{
-      "Data Cannot Inserted";
+      "Data Cannot Updated";
     }
    }
    else{
